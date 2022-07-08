5e014943ace4163fa1fbe5265089ca1823209fbc
Daw Rong <20014009@myrp.edu.sg>
2022-07-08 15:48:33 +0800

    initial commit

--- web/testnew.php
+++ web/testnew.php
@@ -1,10 +1,16 @@
+
+<!-- Have the HTML doctype -->
 <!DOCTYPE html>
+<!--Have a<lang> attributw -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Have a<meta name="viewport"> tag with width or initial-scale-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Have a meta description -->
     <meta name="description" content="C339 Lesson 8 Activity 3 Solution">
+    <!-- Have a <title> element -->
     <title>Activity1</title>
 </head>
 <body>
