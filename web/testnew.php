c3533010f560f662639c89c38f6a68792163c94d
Daw Rong <20014009@myrp.edu.sg>
2022-07-08 12:58:08 +0800

    initial commit

--- web/testnew.php
+++ web/testnew.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <meta name="description" content="C339 Lesson 8 Activity 3 Solution">
+    <title>Activity1</title>
 </head>
 <body>
     <p>Hello World!</p>
